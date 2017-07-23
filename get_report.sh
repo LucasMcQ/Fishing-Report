e2a1e419fb2196b4be36ec5a3bbddb21435c554d
Lucas McQuiston <lmcq12@email.arizona.edu>
2017-07-22 18:06:13 -0700

    Made more clean

--- get_report.sh
+++ get_report.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 MONTH="$(date +%B)"
-DAY="$(date +%d)" #"20"
+DAY="20" #"$(date +%d)" #"20"
 YEAR="$(date +%Y)"
 REPORT_URL="https://www.azgfd.com/fishing-report-$MONTH-$DAY-$YEAR/"
 CHECK_REPORT_URL="https://www.azgfd.com/category/news/"
 
 
-# Before we retrive a specific fishing report, we need to check if there is a 
-# new fishing report. If there is no new fishing report, we will end the script.
-curl $CHECK_REPORT_URL > check_report.txt
+# Check to see if there is a new fishing report present on the AZFGD website.
+curl -s $CHECK_REPORT_URL > check_report.txt
 
 CURRENT_REPORT="$(grep "Fishing Report" check_report.txt | head -1)"
+
 
 # If the current date is not the date on the fishing report, we terminate script.
 if [[ $CURRENT_REPORT != *"$MONTH $DAY, $YEAR"* ]]; then
@@ -19,10 +19,8 @@ if [[ $CURRENT_REPORT != *"$MONTH $DAY, $YEAR"* ]]; then
 fi
 
 
-
-
 # This will retreive the latest fishing reports in Arizona.
-curl $REPORT_URL > fishing_report.txt
+curl -s $REPORT_URL > fishing_report.txt
 
 # Extract the proper lake info from the curl output.
 sed -n '/BARTLETT LAKE/,/<p><b>/p' fishing_report.txt > bartlett_report_uncut.txt
@@ -35,4 +33,4 @@ python send_report.py
 
 
 # Cleanup the mess...
-rm bartlett_report_uncut.txt bartlett_report.txt fishing_report.txt check_report.txt
+rm bartlett_report_uncut.txt bartlett_report.txt fishing_report.txt check_report.txt config.pyc
